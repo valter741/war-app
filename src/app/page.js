9d7a1451dc57a75cbed778e877b77e1fbe62f9a5
Valter <szvalesz98@gmail.com>
2023-05-18 21:24:44 +0200

    1

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,39 @@
+"use client"; 
+
 import Image from 'next/image'
 import styles from './page.module.css'
+import bg from '../../assets/back.jpg'
+import React from 'react';
 
 export default function Home() {
+  const [warCount, setWarCount] = React.useState(0);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className={styles.main} style={{
+      backgroundImage: `url(${bg.src})`,
+      width: '100%',
+      height: '100%',
+    }}>
+        <div className={styles.grid}>
+          <div className={styles.card}  onClick={() => setWarCount(warCount-1)}>
+            <h2>
+              <span>&lt;-</span> Decrease
+            </h2>
+          </div>
+
+          <div className={styles.card3}>
+            <h4>BlackBurn WarCount</h4>
+            <h2>
+              {warCount}
+            </h2>
+          </div>
+
+          <div className={styles.card2} onClick={() => setWarCount(warCount+1)}>
+            <h2>
+              Increase <span>-&gt;</span>
+            </h2>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
