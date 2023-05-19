4a006b48e58c6639a47cb27d671c1729b97e507c
Valter <szvalesz98@gmail.com>
2023-05-19 03:08:53 +0200

    config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async headers() {
+        return [{
+            source: "/:path*",
+            headers: [
+                {
+                    key: "X-Robots-Tag",
+                    value: "noindex",
+                },
+            ],
+        }];
+    }
+}
 
 module.exports = nextConfig
