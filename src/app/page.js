d7b2e256ec36b4854fcd06476969df932fb774bb
Valter <szvalesz98@gmail.com>
2023-05-19 02:24:06 +0200

    deploy 1

--- src/app/page.js
+++ src/app/page.js
@@ -1,39 +1,26 @@
-"use client"; 
+import styles from './page.module.css';
+import bg from '../../assets/back.jpg';
+import Counter from '../comp/counter';
+import { kv } from '@vercel/kv';
 
-import Image from 'next/image'
-import styles from './page.module.css'
-import bg from '../../assets/back.jpg'
-import React from 'react';
 
-export default function Home() {
-  const [warCount, setWarCount] = React.useState(0);
+async function Home() {
+  
 
+  const user = await kv.get('count');
+  
   return (
     <main className={styles.main} style={{
       backgroundImage: `url(${bg.src})`,
-      width: '100%',
-      height: '100%',
+      backgroundPosition: 'top',
+      backgroundRepeat: 'no-repeat',
+      backgroundAttachment: 'fixed',
+      backgroundSize: 'cover',
+      
     }}>
-        <div className={styles.grid}>
-          <div className={styles.card}  onClick={() => setWarCount(warCount-1)}>
-            <h2>
-              <span>&lt;-</span> Decrease
-            </h2>
-          </div>
-
-          <div className={styles.card3}>
-            <h4>BlackBurn WarCount</h4>
-            <h2>
-              {warCount}
-            </h2>
-          </div>
-
-          <div className={styles.card2} onClick={() => setWarCount(warCount+1)}>
-            <h2>
-              Increase <span>-&gt;</span>
-            </h2>
-          </div>
-        </div>
+        <Counter props={user}/>
     </main>
   )
 }
+
+export default Home;
